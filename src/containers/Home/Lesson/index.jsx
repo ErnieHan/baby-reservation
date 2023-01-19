4aaa509362f6f5db1ee9c3a38480995a0df5e4ec
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2023-01-19 14:04:19 +0800

    updates

--- src/containers/Home/Lesson/index.jsx
+++ src/containers/Home/Lesson/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setActiveContainer } from '../../../store/actions/set'
+import Tooltip from '@mui/material/Tooltip'
 
 // icons
 import { Help as HelpIcon } from '@mui/icons-material'
 
 function Lesson() {
+  const dispatch = useDispatch()
+
+  const handleSignUp = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    dispatch(setActiveContainer('signUp'))
+  }
   return (
     <div className="lesson_wrap">
       <div className="notice_container">
@@ -20,7 +29,12 @@ function Lesson() {
         <div className="notice_box">
           <p className="subtitle">
             課程須知
-            <HelpIcon />
+            <Tooltip
+              title="須滿足這些條件再報名參加，方可達最佳效果"
+              placement="top"
+            >
+              <HelpIcon style={{ color: '#303030', margin: '0 4px' }} />
+            </Tooltip>
           </p>
           <p>需自備大浴巾 2 條</p>
           <p>穿著舒適衣褲，修剪指甲、消毒雙手，並配戴口罩</p>
@@ -29,6 +43,28 @@ function Lesson() {
       </div>
       <div className="hr" />
       <h3 className="content_title">教學內容</h3>
+      <p className="desc_text">
+        網頁前端的發展日新月異，隨時掌握最新的標準及開發趨勢至關緊要。本課程的講師迄今仍在業界有密集的實務經驗，確保你與國外資訊零落差，並能夠立即應用在職業中，取得優秀成果。
+      </p>
+      <p className="desc_text">
+        課程中將提及的內容重質也重量，選擇下方的標籤，看看這些你將學到的知識，分屬於前端工程中的哪個知識範疇：
+      </p>
+      <div className="tags_content">
+        <div className="tag">過場動畫</div>
+        <div className="tag">語意化標籤</div>
+        <div className="tag">基礎互動效果</div>
+        <div className="tag">CSS3</div>
+        <div className="tag">Bootstrap</div>
+        <div className="tag">定位及排版技巧</div>
+        <div className="tag">網站部署</div>
+        <div className="tag">Flexbox表單</div>
+        <div className="tag">Grid</div>
+        <div className="tag">雲端字體</div>
+        <div className="tag">瀏覽器行為差異</div>
+        <div className="tag">網格系統</div>
+        <div className="tag">層疊樣式表</div>
+      </div>
+      <div className="hr" />
       <h3 className="content_title">嬰兒按摩的好處</h3>
       <div className="video_content">
         <div className="frame_box">
@@ -41,7 +77,9 @@ function Lesson() {
         </div>
       </div>
       <div className="sign_up_content">
-        <button className="primary_btn">立即預約</button>
+        <button className="sign_up_btn" onClick={handleSignUp}>
+          預約報名
+        </button>
       </div>
     </div>
   )
--- src/containers/Home/Main/index.jsx
+++ src/containers/Home/Main/index.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setActiveContainer } from '../../../store/actions/set'
 
 // icons
 import {
   MessageOutlined as MessageIcon,
-  EmojiFlagsOutlined as FlagIcon,
   CalendarMonth as CalendarIcon
 } from '@mui/icons-material'
-import FormBox from '../../../components/FormBox'
+
+// containers
 import Lesson from '../Lesson'
 
 function Main() {
+  const dispatch = useDispatch()
+  const activeContainer = useSelector(state => state.set.activeContainer)
+
+  const tabs = [
+    {
+      name: '課程說明',
+      value: 'lesson',
+      icon: <MessageIcon style={{ marginRight: '8px' }} />
+    },
+    {
+      name: '預約報名',
+      value: 'signUp',
+      icon: <CalendarIcon style={{ marginRight: '8px' }} />
+    }
+  ]
+
+  const handleTab = value => {
+    dispatch(setActiveContainer(value))
+  }
+
   return (
     <div className="main_container">
       <div className="top_banner">
@@ -20,17 +42,21 @@ function Main() {
         嬰兒按摩，是藉由愛的撫觸來增進父母與嬰兒的關係，讓彼此達到心連心，用雙手、雙眼和心靈去觸動孩子，讓孩子與父母的世界相互連結在一起。
       </h2>
       <div className="switch_buttons_wrap">
-        <button className="tab_btn active">
-          <MessageIcon style={{ marginRight: '8px' }} />
-          課程說明
-        </button>
-        <button className="tab_btn">
-          <CalendarIcon style={{ marginRight: '8px' }} />
-          預約報名
-        </button>
+        {tabs.map((data, index) => (
+          <button
+            key={index}
+            className={`tab_btn ${
+              activeContainer === data.value ? 'active' : ''
+            }`}
+            onClick={() => handleTab(data.value)}
+          >
+            {data.icon}
+            {data.name}
+          </button>
+        ))}
       </div>
-      <Lesson />
-      {/* <FormBox title="基本資料"></FormBox> */}
+      {activeContainer === 'lesson' && <Lesson />}
+      {activeContainer === 'signUp' && <div>signUp</div>}
     </div>
   )
 }
